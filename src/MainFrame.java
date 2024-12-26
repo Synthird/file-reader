3933baa27c6a1490d843f39eca62334e838c7d17
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-26 14:14:32 -0800

    The copied text dialog now has an icon

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -121,7 +121,7 @@ public class MainFrame extends JFrame implements ActionListener {
 			}
 		} else if (e.getSource() == copyButton) {
 			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(textArea.getText()), null);
-			JOptionPane.showMessageDialog(null, "Text copied to clipboard!", "Text copied!", JOptionPane.PLAIN_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Text copied to clipboard!", "Text copied!", JOptionPane.INFORMATION_MESSAGE);
 		} else if (e.getSource() == clearButton) {
 			textArea.setText("");
 		}
